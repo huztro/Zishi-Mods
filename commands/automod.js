ef92f2aff80f5003e50bceb694bea7fe17ad9f19
Huztro <huztroo@gmail.com>
2026-06-03 22:02:58 +0500

    automod.js

--- commands/automod.js
+++ commands/automod.js
@@ -1,1 +1,47 @@
+const {
+    EmbedBuilder,
+    PermissionsBitField
+} = require("discord.js");
 
+module.exports = {
+    name: "automod",
+
+    async execute(message, args) {
+
+        if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+            return message.reply("❌ Missing Permission.");
+        }
+
+        const option = args[0]?.toLowerCase();
+
+        if (!option) {
+            return message.reply(
+                "⚠️ Usage: !automod <on/off>"
+            );
+        }
+
+        if (option === "on") {
+
+            global.automodEnabled = true;
+
+            const embed = new EmbedBuilder()
+                .setColor("Green")
+                .setDescription("✅ AutoMod enabled.");
+
+            return message.reply({ embeds: [embed] });
+        }
+
+        if (option === "off") {
+
+            global.automodEnabled = false;
+
+            const embed = new EmbedBuilder()
+                .setColor("Red")
+                .setDescription("✅ AutoMod disabled.");
+
+            return message.reply({ embeds: [embed] });
+        }
+
+        return message.reply("❌ Invalid option.");
+    }
+};
